a4bd8d1447b707d9d63b0b5e134154a208b927f5
xbexbex <xbexbex@helsinki.fi>
2017-11-17 18:35:37 +0200

    removed useless test data

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,9 +1,3 @@
-INSERT INTO users (username, password) VALUES ('test', 'test');
-INSERT INTO users (username, password) VALUES ('asdf', 'asdf');
-INSERT INTO users (username, password, salt) VALUES ('lol', 
-'6e049c1b44ac596347478665488206ab41471b3bc7ebe7664a583c10e41a41cf992d2d0256cafbfae7134eb21f39c2e8e6ebfb8ade251db4ddccf0801145d613db2dbed8811ef215b22117908991e9834f42a53ef76a74d90ed5a7a0229de147895ca97c737b6377cbdd03d9fc5c572f353eebc312210c22d81c234d96f62ff7',
-'majuriSuolaus');
-
 INSERT INTO mems_data VALUES
 ('00', 'sauce'),
 ('01', 'soda'),
