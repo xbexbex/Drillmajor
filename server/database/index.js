bb07521c12e24c0074ce322760a825d9f0293d36
xbexbex <petja.valkama@gmail.com>
2017-12-23 22:49:29 +0200

    testing heroku

--- server/database/index.js
+++ server/database/index.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg')
 
 const pool = new Pool({
-  user: 'major',
-  host: 'localhost',
-  database: 'major',
-  password: 'major',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
 })
 
 module.exports = {
